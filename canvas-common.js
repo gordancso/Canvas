a278d0a06bd4b6118297c2e228c16777436ebfd2
gordancso <gordan.chungsaion@gmail.com>
2018-01-10 03:15:21 +0800

    update html and CSS

--- canvas-common.js
+++ canvas-common.js
@@ -7,121 +7,127 @@ let dragging = false;
 let mdragging = false;
 let snapshot = [];
 let index = 0;
-snapshot[0] = contextReal.getImageData(0,0, canvasReal.width, canvasReal.height);
+snapshot[0] = contextReal.getImageData(0, 0, canvasReal.width, canvasReal.height);
 
 class PaintFunction {
-    constructor() {
-        contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height)
-    }
-    onMouseDown() { }
-    onDragging() { }
-    onMouseMove() { }
-    onMouseUp() { }
-    onMouseLeave() { }
-    onMouseEnter() { }
-    onDoubleClick() { }
-    reset() {}
-    capture(){
-        snapshot.length = index + 1;
-        snapshot[++index] = contextReal.getImageData(0,0, canvasReal.width, canvasReal.height);
-    }
+  constructor() {
+    contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height)
+  }
+  onMouseDown() {}
+  onDragging() {}
+  onMouseMove() {}
+  onMouseUp() {}
+  onMouseLeave() {}
+  onMouseEnter() {}
+  onDoubleClick() {}
+  reset() {}
+  capture() {
+    snapshot.length = index + 1;
+    snapshot[++index] = contextReal.getImageData(0, 0, canvasReal.width, canvasReal.height);
+  }
 }
 
-function desktopMode(){
-    $('#canvas-draft').mousedown(function (e) {
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseDown([mouseX, mouseY], e);
-        dragging = true;
-    });
+function desktopMode() {
+  $('#canvas-draft').mousedown(function(e) {
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseDown([mouseX, mouseY], e);
+    dragging = true;
+  });
 
-    $('#canvas-draft').mousemove(function (e) {
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        if (dragging) {
-            currentFunction.onDragging([mouseX, mouseY], e);
-        }
-        currentFunction.onMouseMove([mouseX, mouseY], e);
-    });
+  $('#canvas-draft').mousemove(function(e) {
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    if (dragging) {
+      currentFunction.onDragging([mouseX, mouseY], e);
+    }
+    currentFunction.onMouseMove([mouseX, mouseY], e);
+  });
 
-    $('#canvas-draft').mouseup(function (e) {
-        dragging = false;
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseUp([mouseX, mouseY], e);
-    });
+  $('#canvas-draft').mouseup(function(e) {
+    dragging = false;
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseUp([mouseX, mouseY], e);
+  });
 
-    $('#canvas-draft').mouseleave(function (e) {
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseLeave([mouseX, mouseY], e);
-    });
+  $('#canvas-draft').mouseleave(function(e) {
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseLeave([mouseX, mouseY], e);
+  });
 
-    $('#canvas-draft').mouseenter(function (e) {
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onMouseEnter([mouseX, mouseY], e);
-    });
+  $('#canvas-draft').mouseenter(function(e) {
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onMouseEnter([mouseX, mouseY], e);
+  });
 
-    $('#canvas-draft').dblclick(function (e) {
-        let mouseX = e.pageX - this.offsetLeft;
-        let mouseY = e.pageY - this.offsetTop;
-        currentFunction.onDoubleClick([mouseX, mouseY], e);
-    });
+  $('#canvas-draft').dblclick(function(e) {
+    let mouseX = e.pageX - this.offsetLeft;
+    let mouseY = e.pageY - this.offsetTop;
+    currentFunction.onDoubleClick([mouseX, mouseY], e);
+  });
 }
 
-function mobileMode(){
-    var hammertime = new Hammer(canvasDraft);
+function mobileMode() {
+  var hammertime = new Hammer(canvasDraft);
 
-// panup and pandown will not mix with scroll
-    hammertime.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+  // panup and pandown will not mix with scroll
+  hammertime.get('pan').set({
+    direction: Hammer.DIRECTION_ALL
+  });
 
-// swtich between single and double tap
-    hammertime.add(new Hammer.Tap({event: 'doubletap', taps: 2}) );
-    hammertime.add(new Hammer.Tap({event: 'tap'}) );
-    hammertime.get('doubletap').recognizeWith('tap');
-    hammertime.get('tap').requireFailure('doubletap');
+  // swtich between single and double tap
+  hammertime.add(new Hammer.Tap({
+    event: 'doubletap',
+    taps: 2
+  }));
+  hammertime.add(new Hammer.Tap({
+    event: 'tap'
+  }));
+  hammertime.get('doubletap').recognizeWith('tap');
+  hammertime.get('tap').requireFailure('doubletap');
 
-// console.log touch behavior
-    hammertime.on('drag swipe tap doubletap press pan panstart panend panleft panright panmove panup pandown', function(ev) {
+  // console.log touch behavior
+  hammertime.on('drag swipe tap doubletap press pan panstart panend panleft panright panmove panup pandown', function(ev) {
     console.log(ev.type);
-    });
+  });
 
-    hammertime.on('tap panstart',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onMouseDown([mouseX,mouseY],ev);
-        mdragging = true;
-    })
+  hammertime.on('tap panstart', function(ev) {
+    let mouseX = ev.center.x - canvasDraft.offsetLeft;
+    let mouseY = ev.center.y - canvasDraft.offsetTop;
+    currentFunction.onMouseDown([mouseX, mouseY], ev);
+    mdragging = true;
+  })
 
-    hammertime.on('doubletap',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onDoubleClick([mouseX,mouseY],ev);
-    })
+  hammertime.on('doubletap', function(ev) {
+    let mouseX = ev.center.x - canvasDraft.offsetLeft;
+    let mouseY = ev.center.y - canvasDraft.offsetTop;
+    currentFunction.onDoubleClick([mouseX, mouseY], ev);
+  })
 
-    hammertime.on('panmove',function(ev){
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        if (mdragging){
-          currentFunction.onDragging([mouseX,mouseY],ev);
-        }
-        currentFunction.onMouseMove([mouseX,mouseY],ev);
-    });
+  hammertime.on('panmove', function(ev) {
+    let mouseX = ev.center.x - canvasDraft.offsetLeft;
+    let mouseY = ev.center.y - canvasDraft.offsetTop;
+    if (mdragging) {
+      currentFunction.onDragging([mouseX, mouseY], ev);
+    }
+    currentFunction.onMouseMove([mouseX, mouseY], ev);
+  });
 
-    hammertime.on('panend',function(ev){
-        mdragging = false;
-        let mouseX = ev.center.x - canvasDraft.offsetLeft;
-        let mouseY = ev.center.y - canvasDraft.offsetTop;
-        currentFunction.onMouseUp([mouseX,mouseY],ev);
-    });
+  hammertime.on('panend', function(ev) {
+    mdragging = false;
+    let mouseX = ev.center.x - canvasDraft.offsetLeft;
+    let mouseY = ev.center.y - canvasDraft.offsetTop;
+    currentFunction.onMouseUp([mouseX, mouseY], ev);
+  });
 }
 
-$(document).ready(function(){
-    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width()<768) {
-        mobileMode();
-    }
-    else if ($(window).width()>767){
-        desktopMode();
-    }
+$(document).ready(function() {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || $(window).width() < 768) {
+    mobileMode();
+  } else if ($(window).width() > 767) {
+    desktopMode();
+  }
 });
--- drawing-polygon.js
+++ drawing-polygon.js
@@ -1,82 +1,81 @@
 class DrawingPolygon extends PaintFunction {
-    constructor(contextReal, contextDraft) {
-        super();
-        this.contextReal = contextReal;
-        this.contextDraft = contextDraft;
-        this.contextReal.setLineDash([]);
-        this.contextDraft.setLineDash([]);
-        this.contextReal.strokeStyle = this.contextDraft.strokeStyle = "#000";
-        this.contextReal.lineJoin = this.contextDraft.lineJoin = "miter";
-        this.contextReal.lineWidth = this.contextDraft.lineWidth = slider.value;
-        this.reset();
+  constructor(contextReal, contextDraft) {
+    super();
+    this.contextReal = contextReal;
+    this.contextDraft = contextDraft;
+    this.contextReal.setLineDash([]);
+    this.contextDraft.setLineDash([]);
+    this.contextReal.strokeStyle = this.contextDraft.strokeStyle = "#000";
+    this.contextReal.lineJoin = this.contextDraft.lineJoin = "miter";
+    this.contextReal.lineWidth = this.contextDraft.lineWidth = slider.value;
+    this.reset();
 
+  }
+
+  onMouseDown(coord, event) {
+    if (window.screen.availWidth < 767) {
+      this.xArr.push(coord[0]);
+      this.yArr.push(coord[1]);
+      this.draft = true;
     }
+  }
 
-    onMouseDown(coord, event) {
-      if (window.screen.availWidth < 767){
-        this.xArr.push(coord[0]);
-        this.yArr.push(coord[1]);
-        this.draft = true;
-      }
-     }
+  onDragging(coord, event) {}
 
-    onDragging(coord, event) { }
+  onMouseMove(coord, event) {
+    if (this.draft) {
+      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
 
-    onMouseMove(coord, event) {
-        if (this.draft) {
-            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-
-            // check if users click the starting point
-            if (Math.abs(coord[0] - this.xArr[0]) < 20 && Math.abs(coord[1] - this.yArr[0]) < 20)
-                this.redraw(this.contextDraft, this.xArr[0], this.yArr[0]); // yes -> redraw until the starting point
-            else
-                this.redraw(this.contextDraft, coord[0], coord[1]); // no -> redraw until the temporary point
-        }
+      // check if users click the starting point
+      if (Math.abs(coord[0] - this.xArr[0]) < 20 && Math.abs(coord[1] - this.yArr[0]) < 20)
+        this.redraw(this.contextDraft, this.xArr[0], this.yArr[0]); // yes -> redraw until the starting point
+      else
+        this.redraw(this.contextDraft, coord[0], coord[1]); // no -> redraw until the temporary point
     }
+  }
 
-    onMouseUp(coord, event) {
-        // store all the anchor points
-        this.xArr.push(coord[0]);
-        this.yArr.push(coord[1]);
-        console.log(window.screen.availWidth)
+  onMouseUp(coord, event) {
+    // store all the anchor points
+    this.xArr.push(coord[0]);
+    this.yArr.push(coord[1]);
+    console.log(window.screen.availWidth)
 
 
 
-          console.log(this.firstClick)
-        // redraw in real canvas
-        if (this.firstClick) {
-            this.firstClick = false;
-            this.draft = true;
-        }
-        else if (Math.abs(coord[0] - this.xArr[0]) < 20 && Math.abs(coord[1] - this.yArr[0]) < 20) {
-            this.xArr.pop(coord[0]); // remove the final temporary point
-            this.yArr.pop(coord[1]);
-            this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
-            this.redraw(this.contextReal, this.xArr[0], this.yArr[0]);
-            this.capture();
-            this.draft = false;
-            this.reset();
-        }
+    console.log(this.firstClick)
+    // redraw in real canvas
+    if (this.firstClick) {
+      this.firstClick = false;
+      this.draft = true;
+    } else if (Math.abs(coord[0] - this.xArr[0]) < 20 && Math.abs(coord[1] - this.yArr[0]) < 20) {
+      this.xArr.pop(coord[0]); // remove the final temporary point
+      this.yArr.pop(coord[1]);
+      this.contextDraft.clearRect(0, 0, canvasDraft.width, canvasDraft.height);
+      this.redraw(this.contextReal, this.xArr[0], this.yArr[0]);
+      this.capture();
+      this.draft = false;
+      this.reset();
     }
+  }
 
-    onMouseLeave() { }
-    onMouseEnter() { }
+  onMouseLeave() {}
+  onMouseEnter() {}
 
-    // redraw all the points until (x,y)
-    redraw(ctx, x, y) {
-        ctx.beginPath();
-        ctx.fillStyle = rgbaColor;
-        ctx.moveTo(this.xArr[0], this.yArr[0]);
-        for (let i = 0; i < this.xArr.length; i++)
-            ctx.lineTo(this.xArr[i], this.yArr[i]);
-        ctx.lineTo(x, y);
-        ctx.fill();
-        ctx.stroke();
-    }
+  // redraw all the points until (x,y)
+  redraw(ctx, x, y) {
+    ctx.beginPath();
+    ctx.fillStyle = rgbaColor;
+    ctx.moveTo(this.xArr[0], this.yArr[0]);
+    for (let i = 0; i < this.xArr.length; i++)
+      ctx.lineTo(this.xArr[i], this.yArr[i]);
+    ctx.lineTo(x, y);
+    ctx.fill();
+    ctx.stroke();
+  }
 
-    reset() {
-        this.firstClick = true;
-        this.xArr = [];
-        this.yArr = [];
-    }
+  reset() {
+    this.firstClick = true;
+    this.xArr = [];
+    this.yArr = [];
+  }
 }
