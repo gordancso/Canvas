44fc341a8f55f103a732a0762d7d97bb99203b0b
Thomas Lee <thomas.cylee@outlook.com>
2018-01-10 04:21:29 +0800

    updated the responsive canvas

---
+++ main.js
@@ -0,0 +1,47 @@
+$(document).ready(function (){
+    let canvas_width, canvas_height;
+
+    if ($(window).width() < 576){
+        canvas_width = $(window).width();
+        canvas_height = $(window).height() - 100;
+        $('#control-panel').css({'width':`${canvas_width}`,'height':'100px'});
+        $('#canvas-real')[0].width = $('#canvas-draft')[0].width = canvas_width;
+        $('#canvas-real')[0].height = $('#canvas-draft')[0].height = canvas_height;
+        $('#canvas-real').css({'left':'0px', 'top':'100px'});
+        $('#canvas-draft').css({'left':'0px', 'top':'100px'});
+    }
+        
+    else{
+        canvas_width = $(window).width() - 200;
+        canvas_height = $(window).height();
+        $('#control-panel').css({'width': '200px','height':`${canvas_height}`});
+        $('#canvas-real')[0].width = $('#canvas-draft')[0].width = canvas_width;
+        $('#canvas-real')[0].height = $('#canvas-draft')[0].height = canvas_height;
+        $('#canvas-real').css({'left':'200px', 'top':'0px'});
+        $('#canvas-draft').css({'left':'200px', 'top':'0px'});
+    }
+})
+
+$(window).resize(function() {
+    let canvas_width, canvas_height;
+
+    if ($(window).width() < 576){
+        canvas_width = $(window).width();
+        canvas_height = $(window).height() - 100;
+        $('#control-panel').css({'width':`${canvas_width}`,'height':'100px'});
+        $('#canvas-real')[0].width = $('#canvas-draft')[0].width = canvas_width;
+        $('#canvas-real')[0].height = $('#canvas-draft')[0].height = canvas_height;
+        $('#canvas-real').css({'left':'0px', 'top':'100px'});
+        $('#canvas-draft').css({'left':'0px', 'top':'100px'});
+    }
+        
+    else{
+        canvas_width = $(window).width() - 200;
+        canvas_height = $(window).height();
+        $('#control-panel').css({'width': '200px','height':`${canvas_height}`});
+        $('#canvas-real')[0].width = $('#canvas-draft')[0].width = canvas_width;
+        $('#canvas-real')[0].height = $('#canvas-draft')[0].height = canvas_height;
+        $('#canvas-real').css({'left':'200px', 'top':'0px'});
+        $('#canvas-draft').css({'left':'200px', 'top':'0px'});
+    }
+});
